05ef3802cf55d46bd331c87eb9ee188f3802d84c
addiebarron <addiebarronemail@gmail.com>
2020-12-30 18:10:30 -0500

    Polish

--- source/background.js
+++ source/background.js
@@ -1,25 +1,22 @@
 /* Runs in the background across all tabs */
 
+// port for communicating with content/index.js
 let port;
 
-// port for communicating with content/index.js
+// connect to port
 browser.runtime.onConnect.addListener((p) => {
   port = p;
-  console.log('connected on port ' + port.name);
   // listen for changes to tab URLs
   browser.tabs.onUpdated.addListener(onURLChange);
 });
 
+// tell content scripts when their URL changes
 function onURLChange(tabId, changeInfo) {
   const newURL = changeInfo.url;
   if (newURL && /open\.spotify\.com/.test(newURL)) {
-    // when the url changes, notify the content script
     const message = {
-      greeting: 'hi! the URL has changed.',
       url: newURL,
     };
     port.postMessage(message);
-    console.log(`Sent message to tab ${tabId} on port ${port.name}`);
-    console.log(message);
   }
 }
